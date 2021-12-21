e57a82f30ab1bc056a4e91c5dbd0de1763d56e1f
Adriel Felix <adrielandrade3@gmail.com>
2021-12-20 23:15:14 -0300

    update product (http put)

--- src/main/java/com/adriel/springrestcrud/controllers/ProductController.java
+++ src/main/java/com/adriel/springrestcrud/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -41,6 +42,18 @@ public class ProductController {
     @DeleteMapping("/products/{id}")
     public void deleteProduct(@PathVariable Long id){
         productRepository.deleteById(id);
+    }
+
+    @PutMapping("/products/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        return productRepository.findById(id).map(product -> {
+            product.setName(newProduct.getName());
+            product.setPrice(newProduct.getPrice());
+            return productRepository.save(product);
+        }).orElseGet(() -> {
+            newProduct.setId(id);
+            return productRepository.save(newProduct);
+        });
     }
 
 }
